71e4fdf5b6e3d693e57190805b631e86e3fb60c1
Aryanstarx <landgearyan0@gmail.com>
2023-08-21 18:55:42 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,28 @@
+
+
+const parent = React.createElement(
+    'div',
+    { id: "parent" },
+    [React.createElement(
+        'div',
+        { id: "child" },
+        [React.createElement("h1", { class: "heading" }, "I am H1 tag"), React.createElement("h2", { class: "heading" }, "I am H2 tag")]
+    ), React.createElement(
+        'div',
+        { id: "child" },
+        [React.createElement("h1", { class: "heading" }, "I am H1 tag"), React.createElement("h2", { class: "heading" }, "I am H2 tag")]
+    )]
+
+);
+
+
+
+// const heading = React.createElement("h1", { id: "heading" }, "hello World form react");
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+console.log(parent);
+
+root.render(parent);
+
+// the third argument the convert this to an array and give to an array .
